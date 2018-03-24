f0f08084c3cf34f6d9d6066f0f9297b2ecb7c366
Reazul Islam <reazulislam137@gmail.com>
2018-03-24 20:37:06 +0600

    first commit

---
+++ march-24-01.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int radius;
+    float area, perimeter;
+    radius = 6;
+
+    perimeter = 2*3.14*radius;
+    printf("Perimeter of the circle = %f inches\n", perimeter);
+
+    area = 3.14*radius*radius;
+    printf("Area of the circle = %f sqare inches \n",area);
+
+    return 0;
+}
+
